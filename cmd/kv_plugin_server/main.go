d03af4a16100603304766c71936ac9f5077cb9a2
dapeng17951 <327632904@qq.com>
2024-09-22 17:25:43 +0800

    1.修正go-plugin示例； 2.gdb调试；

--- cmd/kv_plugin_server/main.go
+++ cmd/kv_plugin_server/main.go
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: shangke
  * @Date: 2024-09-18 21:48:52
- * @LastEditTime: 2024-09-18 22:17:29
+ * @LastEditTime: 2024-09-22 17:21:23
  * @LastEditors: shangke
  */
 package main
@@ -11,20 +11,21 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"wireDemo/shared/kv"
 
 	"github.com/hashicorp/go-plugin"
 )
 
-func run() error {
+func run(path string) error {
 	// We're a host. Start by launching the plugin process.
 	client := plugin.NewClient(&plugin.ClientConfig{
 		HandshakeConfig: kv.Handshake,
 		Plugins:         kv.PluginMap,
 		//Cmd:             exec.Command("sh", "-c", os.Getenv("KV_PLUGIN")), //调用 plugin 进程，这里使用环境变量执行的
-		Cmd: exec.Command("sh", "-c", "client.exe"),
+		Cmd: exec.Command(path + "\\" + "client.exe"),
 		AllowedProtocols: []plugin.Protocol{
-			plugin.ProtocolNetRPC, plugin.ProtocolGRPC},
+			plugin.ProtocolGRPC},
 	})
 	defer client.Kill()
 
@@ -43,34 +44,34 @@ func run() error {
 	// We should have a KV store now! This feels like a normal interface
 	// implementation but is in fact over an RPC connection.
 	kv := raw.(kv.KV)
-	os.Args = os.Args[1:]
-	switch os.Args[0] {
-	case "get":
-		result, err := kv.Get(os.Args[1])
-		if err != nil {
-			return err
-		}
 
-		fmt.Println(string(result))
-
-	case "put":
-		err := kv.Put(os.Args[1], []byte(os.Args[2]))
-		if err != nil {
-			return err
-		}
-
-	default:
-		return fmt.Errorf("Please only use 'get' or 'put', given: %q", os.Args[0])
+	result, err := kv.Get("1")
+	if err != nil {
+		return err
 	}
 
-	return nil
+	fmt.Println(string(result))
+
+	err2 := kv.Put("1", []byte("2"))
+	if err2 != nil {
+		return err2
+	}
+
+	return fmt.Errorf("Please only use 'get' or 'put', given: %q", "1")
 }
 
 // 服务端
 func main() {
 	// We don't want to see the plugin logs.
+	ex, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	exPath := filepath.Dir(ex)
 
-	if err := run(); err != nil {
+	//https://learnku.com/go/wikis/23398
+
+	if err := run(exPath); err != nil {
 		fmt.Printf("error: %+v\n", err)
 		os.Exit(1)
 	}
